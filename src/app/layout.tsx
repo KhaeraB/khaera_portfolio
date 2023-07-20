8a6f51f5db14789aabc3deb29ae172ffe4446e90
BELKADI <khaera/belkadi@gmail.com>
2023-07-21 00:09:34 +0200

    prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
-'use client';
-import './../styles/global.css';
-import type { Metadata } from 'next';
-import NavBar from '../components/NavBar';
-import { ThemeProvider } from 'next-themes';
+"use client";
+import "./../styles/global.css";
+import type { Metadata } from "next";
+import NavBar from "../components/NavBar";
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
   icons: {
-    icon: '/favicon.ico',
+    icon: "/favicon.ico",
   },
-  title: 'Khaera_blk',
-  description: 'Portfolio of my projects',
+  title: "Khaera_blk",
+  description: "Portfolio of my projects",
 };
 
 export default function RootLayout({
@@ -18,10 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className='dark:bg-stone-900'>
-        <ThemeProvider enableSystem={true} attribute='class'>
-          {' '}
+    <html lang="en">
+      <body className="dark:bg-stone-900">
+        <ThemeProvider enableSystem={true} attribute="class">
           <NavBar />
           {children}
         </ThemeProvider>
