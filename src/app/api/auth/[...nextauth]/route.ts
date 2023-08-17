81ab9d1aea89b2d503067f4f51f7c808e46a751a
BELKADI <khaera/belkadi@gmail.com>
2023-08-18 00:36:51 +0200

    log

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -13,13 +13,6 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET ?? "",
     }),
   ],
-  pages: {
-    signIn: "api/auth/signin",
-    signOut: "api/auth/signout",
-    error: "api/auth/error", // Error code passed in query string as ?error=
-    verifyRequest: "api/auth/verify-request", // (used for check email message)
-    newUser: "api/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
-  },
 });
 
 export { handler as GET, handler as POST };
