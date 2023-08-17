9a1ea2ad7ac1a344f7a55096f81234a9baa81dc1
BELKADI <khaera/belkadi@gmail.com>
2023-08-18 00:35:39 +0200

    Revert "loging"
    This reverts commit 8a798307dad45e0b52c33c2cbcf45aead13c6fda.

--- src/app/dashboard/login/page.tsx
+++ src/app/dashboard/login/page.tsx
@@ -1,13 +1,22 @@
 "use client";
-import OAuthComponent from "@/components/OauthComponent/OauthCpmponent";
 import { signIn } from "next-auth/react";
 import React from "react";
 
 const page = () => {
   return (
     <div className="flex flex-row justify-around">
-      <OAuthComponent company="Github" />
-      <OAuthComponent company="Google" />
+      <button
+        className="p-2 m-2 rounded bg-yellow-600 text-white"
+        onClick={() => signIn("google")}
+      >
+        Login with Google
+      </button>
+      <button
+        className="p-2 m-2 rounded bg-stone-800 text-white"
+        onClick={() => signIn("github")}
+      >
+        Login with Github
+      </button>
     </div>
   );
 };
